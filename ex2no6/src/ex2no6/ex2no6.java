153a4eaf6be080d273170f546efd124c32a4fa3c
admin <admin@DESKTOP-8L2IG92>
2021-03-29 20:48:09 +0900

    ex2no6

---
+++ ex2no6/src/ex2no6/ex2no6.java
@@ -0,0 +1,20 @@
+package ex2no6;
+
+import java.util.Scanner;
+
+public class ex2no6 {   //41p
+     public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("?‘?ë¦? ? •?ˆ˜ë¥? ?•˜?‚˜ ?…? ¥?•´ì£¼ì„¸?š”.>");
+    	String input = scanner.nextLine();
+    	int num = Integer.parseInt(input); //?…? ¥ ë°›ì? ë¬¸ì?—´?„ ?ˆ«?ë¡? ë³??™˜
+    	
+    	System.out.println("?…? ¥?‚´?š© :"+input);
+    	System.out.printf("num=%d%n", num);
+    	 
+    	 
+	}
+	
+	
+}
